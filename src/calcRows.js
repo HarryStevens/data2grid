bb30419bd2cea7af4d830c88384358e7bfc183ec
HarryStevens <harryjosephstevens@gmail.com>
2017-12-02 11:05:20 +0530

    Cleaner code, better errors

--- src/calcRows.js
+++ src/calcRows.js
@@ -1,17 +1,17 @@
 // Calculates the optimal number of rows for a number of data points.
 export default function calcRows(number){
 
-	// errors
+  // errors
 
-	// the number argument is not declared
-	if (!number) {
-		throw new Error("You must pass a number.");
-	}
+  // the number argument is not declared
+  if (!number) {
+    throw new Error("You must pass a number.");
+  }
 
-	// the number argument is declared but it is not a positive integer
-	if (number && (parseInt(number) !== number || Number(number) <= 0)) {
-		throw new Error("Rows must be a positive integer.");
-	}
+  // the number argument is declared but it is not a positive integer
+  if (number && (parseInt(number) !== number || Number(number) <= 0)) {
+    throw new Error("Rows must be a positive integer.");
+  }
 
-	return Math.ceil(Math.sqrt(number));
+  return Math.ceil(Math.sqrt(number));
 }
